ca05fc9ba97f844c5ab8e53e4623d7d1b1cf69cc
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-07-15 01:39:35 +0200

    Add math header to library

--- src/lib/neo/convolution.hpp
+++ src/lib/neo/convolution.hpp
@@ -2,3 +2,4 @@
 
 #include "neo/convolution/container.hpp"
 #include "neo/convolution/convolver.hpp"
+#include "neo/convolution/math.hpp"
---
+++ src/lib/neo/convolution/math.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "neo/convolution/math/fixed_point.hpp"
