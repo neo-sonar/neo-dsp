d6ad66c49ec321b03297d041c3ee7de7f1722e20
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-07-15 13:18:44 +0200

    Start complex_q15_t

--- src/lib/neo/convolution/math/fixed_point.hpp
+++ src/lib/neo/convolution/math/fixed_point.hpp
@@ -13,6 +13,7 @@
 #endif
 
 #include <algorithm>
+#include <array>
 #include <cassert>
 #include <concepts>
 #include <cstdint>
@@ -59,7 +60,6 @@ inline constexpr auto apply_fixed_point_kernel_sse
 template<int ValueSizeBits>
 inline constexpr auto apply_fixed_point_kernel_neon128
     = [](auto const& lhs, auto const& rhs, auto const& out, auto scalar_kernel, auto vector_kernel) {
-
     auto load = [](auto* ptr) {
         if constexpr (ValueSizeBits == 8) {
             return vld1q_s8(reinterpret_cast<std::int8_t const*>(ptr));
@@ -338,5 +338,48 @@ auto multiply(
         }
     }
 }
+
+struct complex_q15_t
+{
+    using value_type = q15_t;
+
+    constexpr complex_q15_t() = default;
+
+    constexpr complex_q15_t(q15_t re, q15_t im = q15_t{}) noexcept : _data{re, im} {}
+
+    [[nodiscard]] constexpr auto real() const noexcept -> q15_t { return _data[0]; }
+
+    [[nodiscard]] constexpr auto imag() const noexcept -> q15_t { return _data[1]; }
+
+    constexpr auto real(q15_t re) noexcept -> void { _data[0] = re; }
+
+    constexpr auto imag(q15_t im) noexcept -> void { _data[1] = im; }
+
+    friend constexpr auto operator+(complex_q15_t lhs, complex_q15_t rhs) -> complex_q15_t
+    {
+        return complex_q15_t{lhs.real() + rhs.real(), lhs.imag() + rhs.imag()};
+    }
+
+    friend constexpr auto operator-(complex_q15_t lhs, complex_q15_t rhs) -> complex_q15_t
+    {
+        return complex_q15_t{lhs.real() - rhs.real(), lhs.imag() - rhs.imag()};
+    }
+
+    friend constexpr auto operator*(complex_q15_t lhs, complex_q15_t rhs) -> complex_q15_t
+    {
+        auto const a = lhs.real().value();
+        auto const b = lhs.imag().value();
+        auto const c = rhs.real().value();
+        auto const d = lhs.imag().value();
+
+        return complex_q15_t{
+            {underlying_value, static_cast<std::int16_t>(((a * c) >> 17) - ((b * d) >> 17))},
+            {underlying_value, static_cast<std::int16_t>(((a * d) >> 17) + ((b * c) >> 17))},
+        };
+    }
+
+private:
+    std::array<q15_t, 2> _data{};
+};
 
 }  // namespace neo::fft
