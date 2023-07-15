3dc828bf521d76b471f478f8e534bb8d43651fdf
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-07-15 08:36:44 +0200

    Cleanup fixed_point multiply

--- src/lib/neo/convolution/math/fixed_point.hpp
+++ src/lib/neo/convolution/math/fixed_point.hpp
@@ -225,52 +225,56 @@ auto multiply(
     assert(lhs.size() == rhs.size());
     assert(lhs.size() == out.size());
 
-    if constexpr (std::same_as<StorageType, std::int8_t>) {
-#if defined(__SSE4_1__)
-        auto const kernel = [](__m128i left, __m128i right) -> __m128i {
-            auto const lowLeft    = _mm_cvtepi8_epi16(left);
-            auto const lowRight   = _mm_cvtepi8_epi16(right);
-            auto const lowProduct = _mm_mullo_epi16(lowLeft, lowRight);
-            auto const lowShifted = _mm_srli_epi16(lowProduct, FractionalBits);
-
-            auto const highLeft    = _mm_cvtepi8_epi16(_mm_srli_si128(left, 8));
-            auto const highRight   = _mm_cvtepi8_epi16(_mm_srli_si128(right, 8));
-            auto const highProduct = _mm_mullo_epi16(highLeft, highRight);
-            auto const highShifted = _mm_srli_epi16(highProduct, FractionalBits);
-
-            return _mm_packs_epi16(lowShifted, highShifted);
-        };
-        detail::apply_fixed_point_kernel_sse<8>(lhs, rhs, out, std::multiplies{}, kernel);
-#else
-        for (auto i{0U}; i < lhs.size(); ++i) { out[i] = std::multiplies{}(lhs[i], rhs[i]); }
-#endif
-    } else if constexpr (std::same_as<StorageType, std::int16_t>) {
-#if 0  // defined(__SSE4_1__)
-        auto const kernel = [](__m128i left, __m128i right) -> __m128i
-        {
-            auto const lowLeft    = _mm_cvtepi16_epi32(left);
-            auto const lowRight   = _mm_cvtepi16_epi32(right);
-            auto const lowProduct = _mm_mullo_epi32(lowLeft, lowRight);
-            auto const lowShifted = _mm_srli_epi32(lowProduct, FractionalBits);
-
-            auto const highLeft    = _mm_cvtepi16_epi32(_mm_srli_si128(left, 8));
-            auto const highRight   = _mm_cvtepi16_epi32(_mm_srli_si128(right, 8));
-            auto const highProduct = _mm_mullo_epi32(highLeft, highRight);
-            auto const highShifted = _mm_srli_epi32(highProduct, FractionalBits);
-
-            return _mm_packs_epi32(lowShifted, highShifted);
-        };
-
-        detail::apply_fixed_point_kernel_sse<16>(lhs, rhs, out, std::multiplies{}, kernel);
-#elif defined(__SSE3__)
+    if constexpr (std::same_as<StorageType, std::int16_t> && FractionalBits == 15) {
+#if defined(__SSE3__)
         auto const kernel = [](__m128i left, __m128i right) -> __m128i { return _mm_mulhrs_epi16(left, right); };
         detail::apply_fixed_point_kernel_sse<16>(lhs, rhs, out, std::multiplies{}, kernel);
 #else
         for (auto i{0U}; i < lhs.size(); ++i) { out[i] = std::multiplies{}(lhs[i], rhs[i]); }
 #endif
-
     } else {
-        for (auto i{0U}; i < lhs.size(); ++i) { out[i] = std::minus{}(lhs[i], rhs[i]); }
+        if constexpr (std::same_as<StorageType, std::int8_t>) {
+#if defined(__SSE4_1__)
+            auto const kernel = [](__m128i left, __m128i right) -> __m128i {
+                auto const lowLeft    = _mm_cvtepi8_epi16(left);
+                auto const lowRight   = _mm_cvtepi8_epi16(right);
+                auto const lowProduct = _mm_mullo_epi16(lowLeft, lowRight);
+                auto const lowShifted = _mm_srli_epi16(lowProduct, FractionalBits);
+
+                auto const highLeft    = _mm_cvtepi8_epi16(_mm_srli_si128(left, 8));
+                auto const highRight   = _mm_cvtepi8_epi16(_mm_srli_si128(right, 8));
+                auto const highProduct = _mm_mullo_epi16(highLeft, highRight);
+                auto const highShifted = _mm_srli_epi16(highProduct, FractionalBits);
+
+                return _mm_packs_epi16(lowShifted, highShifted);
+            };
+            detail::apply_fixed_point_kernel_sse<8>(lhs, rhs, out, std::multiplies{}, kernel);
+#else
+            for (auto i{0U}; i < lhs.size(); ++i) { out[i] = std::multiplies{}(lhs[i], rhs[i]); }
+#endif
+        } else if constexpr (std::same_as<StorageType, std::int16_t>) {
+#if defined(__SSE4_1__)
+            auto const kernel = [](__m128i left, __m128i right) -> __m128i {
+                auto const lowLeft    = _mm_cvtepi16_epi32(left);
+                auto const lowRight   = _mm_cvtepi16_epi32(right);
+                auto const lowProduct = _mm_mullo_epi32(lowLeft, lowRight);
+                auto const lowShifted = _mm_srli_epi32(lowProduct, FractionalBits);
+
+                auto const highLeft    = _mm_cvtepi16_epi32(_mm_srli_si128(left, 8));
+                auto const highRight   = _mm_cvtepi16_epi32(_mm_srli_si128(right, 8));
+                auto const highProduct = _mm_mullo_epi32(highLeft, highRight);
+                auto const highShifted = _mm_srli_epi32(highProduct, FractionalBits);
+
+                return _mm_packs_epi32(lowShifted, highShifted);
+            };
+
+            detail::apply_fixed_point_kernel_sse<16>(lhs, rhs, out, std::multiplies{}, kernel);
+#else
+            for (auto i{0U}; i < lhs.size(); ++i) { out[i] = std::multiplies{}(lhs[i], rhs[i]); }
+#endif
+        } else {
+            for (auto i{0U}; i < lhs.size(); ++i) { out[i] = std::multiplies{}(lhs[i], rhs[i]); }
+        }
     }
 }
 
