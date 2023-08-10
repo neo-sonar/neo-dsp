9a7e564b00aef14142491ca8f814f7c8f405c493
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-08-10 02:58:51 +0200

    Fix stereo_upols_convolver

--- src/lib/neo/fft/convolution/upols_convolver.cpp
+++ src/lib/neo/fft/convolution/upols_convolver.cpp
@@ -9,6 +9,24 @@
 
 namespace neo::fft {
 
+static auto copy_to_window(
+    Kokkos::mdspan<float const, Kokkos::dextents<size_t, 2>> src,
+    Kokkos::mdspan<float, Kokkos::dextents<size_t, 2>> dest
+) -> void
+{
+    assert(src.extent(0) == dest.extent(0));
+    assert(src.extent(1) * 2 == dest.extent(1));
+
+    auto const numChannels = src.extent(0);
+    auto const numSamples  = src.extent(1);
+
+    for (auto ch{0UL}; ch < numChannels; ++ch) {
+        auto source      = std::span{std::addressof(src(ch, 0)), numSamples};
+        auto destination = std::span{std::addressof(dest(ch, numSamples)), numSamples};
+        std::copy(source.begin(), source.end(), destination.begin());
+    }
+}
+
 static auto multiply_and_accumulate_row(
     Kokkos::mdspan<std::complex<float> const, Kokkos::dextents<std::size_t, 1>> lhs,
     Kokkos::mdspan<std::complex<float> const, Kokkos::dextents<std::size_t, 1>> rhs,
@@ -152,7 +170,7 @@ auto stereo_upols_convolver::operator()(KokkosEx::mdspan<float, Kokkos::dextents
     // Time domain input buffer
     auto const window = _window.to_mdspan();
     shift_rows_left(window, blockSize);
-    copy(block, window);
+    copy_to_window(block, window);
 
     // 2B-point R2C-FFT
     // Copy to FDL
