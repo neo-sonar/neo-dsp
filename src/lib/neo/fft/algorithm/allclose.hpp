042f9ccaaa09a1279b00c4ef039369b1184b84ae
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-08-11 21:21:45 +0200

    Add tolerance parameter to allclose

--- src/lib/neo/fft/algorithm/allclose.hpp
+++ src/lib/neo/fft/algorithm/allclose.hpp
@@ -8,21 +8,9 @@
 
 namespace neo::fft {
 
-template<in_vector InVec1, in_vector InVec2>
-[[nodiscard]] auto allclose(InVec1 lhs, InVec2 rhs) -> bool
+template<in_vector InVec1, in_vector InVec2, typename Scalar>
+[[nodiscard]] auto allclose(InVec1 lhs, InVec2 rhs, Scalar tolerance) -> bool
 {
-    using Left  = typename InVec1::value_type;
-    using Right = typename InVec2::value_type;
-    using Float = decltype(std::abs(std::declval<Left>() - std::declval<Right>()));
-    static_assert(std::floating_point<Float>);
-
-    auto const tolerance = [] {
-        if constexpr (std::same_as<Float, float>) {
-            return Float(1e-5);
-        }
-        return Float(1e-9);
-    }();
-
     if (lhs.extents() != rhs.extents()) {
         return false;
     }
@@ -34,6 +22,24 @@ template<in_vector InVec1, in_vector InVec2>
     }
 
     return true;
+}
+
+template<in_vector InVec1, in_vector InVec2>
+[[nodiscard]] auto allclose(InVec1 lhs, InVec2 rhs) -> bool
+{
+    auto const tolerance = [] {
+        using Left  = typename InVec1::value_type;
+        using Right = typename InVec2::value_type;
+        using Float = decltype(std::abs(std::declval<Left>() - std::declval<Right>()));
+        static_assert(std::floating_point<Float>);
+
+        if constexpr (std::same_as<Float, float>) {
+            return Float(1e-5);
+        }
+        return Float(1e-9);
+    }();
+
+    return allclose(lhs, rhs, tolerance);
 }
 
 }  // namespace neo::fft
--- src/lib/neo/fft/algorithm/allclose_test.cpp
+++ src/lib/neo/fft/algorithm/allclose_test.cpp
@@ -5,9 +5,19 @@
 #include <catch2/catch_template_test_macros.hpp>
 #include <catch2/generators/catch_generators.hpp>
 
+template<typename T>
+static constexpr auto floatOrComplexValueType()
+{
+    if constexpr (std::floating_point<T>) {
+        return T{};
+    } else {
+        return typename T::value_type{};
+    }
+}
+
 TEMPLATE_TEST_CASE("neo/fft/algorithm: allclose(in_vector)", "", float, double, std::complex<float>, std::complex<double>)
 {
-    using Float = TestType;
+    using Float = decltype(floatOrComplexValueType<TestType>());
 
     auto const size       = GENERATE(as<std::size_t>{}, 1, 2, 33, 128);
     auto const makeVector = [size](Float val) {
@@ -16,12 +26,14 @@ TEMPLATE_TEST_CASE("neo/fft/algorithm: allclose(in_vector)", "", float, double, 
         return vec;
     };
 
-    auto const zeros = makeVector(Float(0));
-    auto const ones  = makeVector(Float(1));
+    auto const zeros      = makeVector(Float(0));
+    auto const ones       = makeVector(Float(1));
+    auto const almostOnes = makeVector(Float(0.95));
+
+    REQUIRE_FALSE(neo::fft::allclose(ones.to_mdspan(), zeros.to_mdspan()));
+    REQUIRE_FALSE(neo::fft::allclose(ones.to_mdspan(), almostOnes.to_mdspan()));
 
     REQUIRE(neo::fft::allclose(zeros.to_mdspan(), zeros.to_mdspan()));
     REQUIRE(neo::fft::allclose(ones.to_mdspan(), ones.to_mdspan()));
-
-    REQUIRE_FALSE(neo::fft::allclose(zeros.to_mdspan(), ones.to_mdspan()));
-    REQUIRE_FALSE(neo::fft::allclose(ones.to_mdspan(), zeros.to_mdspan()));
+    REQUIRE(neo::fft::allclose(ones.to_mdspan(), almostOnes.to_mdspan(), Float(0.7)));
 }
