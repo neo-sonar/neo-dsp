4c4515f03965ba2dca81e956905c9eee568e8a89
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-08-11 23:02:51 +0200

    Improve sparse_matrix tests

--- src/lib/neo/fft/container/sparse_matrix_test.cpp
+++ src/lib/neo/fft/container/sparse_matrix_test.cpp
@@ -22,7 +22,21 @@ TEMPLATE_TEST_CASE("neo/fft/container: sparse_matrix", "", float, double)
 
     auto row = std::vector<Float>(lhs.extent(1));
     std::fill(row.begin(), row.end(), Float(2));
+
     other.insert_row(0, std::span{row}, [](auto, auto, auto) { return true; });
     REQUIRE(other.value_container().size() == 32);
     REQUIRE(other.column_container().size() == 32);
+
+    other.insert_row(1, std::span{row}, [](auto, auto col, auto) { return col % 2U == 0; });
+    REQUIRE(other.value_container().size() == 48);
+    REQUIRE(other.column_container().size() == 48);
+
+    auto const half = lhs.extent(1) / 2;
+    other.insert_row(1, std::span{row}, [half](auto, auto col, auto) { return col < half; });
+    REQUIRE(std::equal_to{}(other(1, half), Float(0)));
+    REQUIRE(std::equal_to{}(other(1, half - 1), Float(2)));
+
+    other.insert_row(1, std::span{row}, [](auto, auto, auto) { return true; });
+    REQUIRE(other.value_container().size() == 64);
+    REQUIRE(other.column_container().size() == 64);
 }
