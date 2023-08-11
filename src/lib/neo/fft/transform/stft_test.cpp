a8cb7b9048be577cdafc50f4b574c74fb6e64f80
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-08-11 19:18:51 +0200

    Add basic stft tests

---
+++ src/lib/neo/fft/transform/stft_test.cpp
@@ -0,0 +1,25 @@
+#include "stft.hpp"
+
+#include <catch2/catch_approx.hpp>
+#include <catch2/catch_template_test_macros.hpp>
+
+TEMPLATE_TEST_CASE("neo/fft/transform/stft: stft", "", float, double)
+{
+    using Float = TestType;
+
+    SECTION("no remainder")
+    {
+        auto signal = KokkosEx::mdarray<Float, Kokkos::dextents<std::size_t, 2>>{1, 2048};
+        auto frames = neo::fft::stft(signal.to_mdspan(), 256);
+        REQUIRE(frames.extent(0) == 8);
+        REQUIRE(frames.extent(1) == 129);
+    }
+
+    SECTION("with remainder")
+    {
+        auto signal = KokkosEx::mdarray<Float, Kokkos::dextents<std::size_t, 2>>{1, 2040};
+        auto frames = neo::fft::stft(signal.to_mdspan(), 256);
+        REQUIRE(frames.extent(0) == 8);
+        REQUIRE(frames.extent(1) == 129);
+    }
+}
