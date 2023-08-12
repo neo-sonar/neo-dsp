e1aa8b8c8da05699c16b6b3ccf441c32fbb84c12
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-08-12 02:43:53 +0200

    Improve fixed_point tests

--- src/lib/neo/fft/fixed_point/fixed_point_test.cpp
+++ src/lib/neo/fft/fixed_point/fixed_point_test.cpp
@@ -2,6 +2,7 @@
 #include "complex.hpp"
 #include "fixed_point.hpp"
 
+#include <catch2/catch_template_test_macros.hpp>
 #include <catch2/catch_test_macros.hpp>
 
 #include <functional>
@@ -14,44 +15,35 @@
     return std::abs(a - b) < tolerance;
 }
 
-[[nodiscard]] static auto test_q7_t_conversion() -> bool
+template<typename T>
+static constexpr auto tolerance = [] {
+    if constexpr (std::same_as<T, neo::fft::q7_t>) {
+        return 0.01F;
+    }
+    return 0.0001F;
+}();
+
+TEMPLATE_TEST_CASE("neo/fft/fixed_point: to_float(fixed_point)", "", neo::fft::q7_t, neo::fft::q15_t)
 {
-    auto const tolerance = 0.01F;
-    REQUIRE(approx_equal(to_float(neo::fft::q7_t{0.00F}), 0.00F, tolerance));
-    REQUIRE(approx_equal(to_float(neo::fft::q7_t{0.12F}), 0.12F, tolerance));
-    REQUIRE(approx_equal(to_float(neo::fft::q7_t{0.25F}), 0.25F, tolerance));
-    REQUIRE(approx_equal(to_float(neo::fft::q7_t{0.33F}), 0.33F, tolerance));
-    REQUIRE(approx_equal(to_float(neo::fft::q7_t{0.40F}), 0.40F, tolerance));
-    REQUIRE(approx_equal(to_float(neo::fft::q7_t{0.50F}), 0.50F, tolerance));
-    REQUIRE(approx_equal(to_float(neo::fft::q7_t{0.75F}), 0.75F, tolerance));
-    return true;
+    using fixed_point_t = TestType;
+
+    REQUIRE(approx_equal(to_float(fixed_point_t{0.00F}), 0.00F, tolerance<fixed_point_t>));
+    REQUIRE(approx_equal(to_float(fixed_point_t{0.12F}), 0.12F, tolerance<fixed_point_t>));
+    REQUIRE(approx_equal(to_float(fixed_point_t{0.25F}), 0.25F, tolerance<fixed_point_t>));
+    REQUIRE(approx_equal(to_float(fixed_point_t{0.33F}), 0.33F, tolerance<fixed_point_t>));
+    REQUIRE(approx_equal(to_float(fixed_point_t{0.40F}), 0.40F, tolerance<fixed_point_t>));
+    REQUIRE(approx_equal(to_float(fixed_point_t{0.50F}), 0.50F, tolerance<fixed_point_t>));
+    REQUIRE(approx_equal(to_float(fixed_point_t{0.75F}), 0.75F, tolerance<fixed_point_t>));
 }
 
-[[nodiscard]] static auto test_q15_t_conversion() -> bool
+TEMPLATE_TEST_CASE("neo/fft/fixed_point: unary_op(fixed_point)", "", neo::fft::q7_t, neo::fft::q15_t)
 {
-    using fxp_t          = neo::fft::q15_t;
-    auto const tolerance = 0.0001F;
+    using fixed_point_t = TestType;
 
-    REQUIRE(approx_equal(to_float(fxp_t{0.00F}), 0.00F, tolerance));
-    REQUIRE(approx_equal(to_float(fxp_t{0.12F}), 0.12F, tolerance));
-    REQUIRE(approx_equal(to_float(fxp_t{0.25F}), 0.25F, tolerance));
-    REQUIRE(approx_equal(to_float(fxp_t{0.33F}), 0.33F, tolerance));
-    REQUIRE(approx_equal(to_float(fxp_t{0.40F}), 0.40F, tolerance));
-    REQUIRE(approx_equal(to_float(fxp_t{0.50F}), 0.50F, tolerance));
-    REQUIRE(approx_equal(to_float(fxp_t{0.75F}), 0.75F, tolerance));
-
-    return true;
-}
-
-[[nodiscard]] static auto test_q7_t_unary_ops() -> bool
-{
-    using fxp_t          = neo::fft::q7_t;
-    auto const tolerance = 0.01F;
-
-    auto unary_op = [tolerance](auto val, auto op) {
-        auto const result   = to_float(op(fxp_t{val}));
+    auto unary_op = [](auto val, auto op) {
+        auto const result   = to_float(op(fixed_point_t{val}));
         auto const expected = op(val);
-        return approx_equal(result, expected, tolerance);
+        return approx_equal(result, expected, tolerance<fixed_point_t>);
     };
 
     // operator+
@@ -96,76 +88,16 @@
     REQUIRE(unary_op(-0.49F, std::negate()));
     REQUIRE(unary_op(-0.75F, std::negate()));
     REQUIRE(unary_op(-0.99F, std::negate()));
-
-    return true;
 }
 
-[[nodiscard]] static auto test_q15_t_unary_ops() -> bool
+TEMPLATE_TEST_CASE("neo/fft/fixed_point: binary_op(fixed_point)", "", neo::fft::q7_t, neo::fft::q15_t)
 {
-    using fxp_t          = neo::fft::q15_t;
-    auto const tolerance = 0.0001F;
+    using fixed_point_t = TestType;
 
-    auto unary_op = [tolerance](auto val, auto op) {
-        auto const result   = to_float(op(fxp_t{val}));
-        auto const expected = op(val);
-        return approx_equal(result, expected, tolerance);
-    };
-
-    // operator+
-    auto const unary_plus = [](auto val) { return +val; };
-    REQUIRE(unary_op(0.00F, unary_plus));
-    REQUIRE(unary_op(0.12F, unary_plus));
-    REQUIRE(unary_op(0.25F, unary_plus));
-    REQUIRE(unary_op(0.33F, unary_plus));
-    REQUIRE(unary_op(0.40F, unary_plus));
-    REQUIRE(unary_op(0.45F, unary_plus));
-    REQUIRE(unary_op(0.49F, unary_plus));
-    REQUIRE(unary_op(0.75F, unary_plus));
-    REQUIRE(unary_op(0.99F, unary_plus));
-
-    REQUIRE(unary_op(-0.00F, unary_plus));
-    REQUIRE(unary_op(-0.12F, unary_plus));
-    REQUIRE(unary_op(-0.25F, unary_plus));
-    REQUIRE(unary_op(-0.33F, unary_plus));
-    REQUIRE(unary_op(-0.40F, unary_plus));
-    REQUIRE(unary_op(-0.45F, unary_plus));
-    REQUIRE(unary_op(-0.49F, unary_plus));
-    REQUIRE(unary_op(-0.75F, unary_plus));
-    REQUIRE(unary_op(-0.99F, unary_plus));
-
-    // operator-
-    REQUIRE(unary_op(0.00F, std::negate()));
-    REQUIRE(unary_op(0.12F, std::negate()));
-    REQUIRE(unary_op(0.25F, std::negate()));
-    REQUIRE(unary_op(0.33F, std::negate()));
-    REQUIRE(unary_op(0.40F, std::negate()));
-    REQUIRE(unary_op(0.45F, std::negate()));
-    REQUIRE(unary_op(0.49F, std::negate()));
-    REQUIRE(unary_op(0.75F, std::negate()));
-    REQUIRE(unary_op(0.99F, std::negate()));
-
-    REQUIRE(unary_op(-0.00F, std::negate()));
-    REQUIRE(unary_op(-0.12F, std::negate()));
-    REQUIRE(unary_op(-0.25F, std::negate()));
-    REQUIRE(unary_op(-0.33F, std::negate()));
-    REQUIRE(unary_op(-0.40F, std::negate()));
-    REQUIRE(unary_op(-0.45F, std::negate()));
-    REQUIRE(unary_op(-0.49F, std::negate()));
-    REQUIRE(unary_op(-0.75F, std::negate()));
-    REQUIRE(unary_op(-0.99F, std::negate()));
-
-    return true;
-}
-
-[[nodiscard]] static auto test_q7_t_binary_ops() -> bool
-{
-    using fxp_t          = neo::fft::q7_t;
-    auto const tolerance = 0.01F;
-
-    auto binary_op = [tolerance](auto lhs, auto rhs, auto op) {
-        auto const result   = to_float(op(fxp_t{lhs}, fxp_t{rhs}));
+    auto binary_op = [](auto lhs, auto rhs, auto op) {
+        auto const result   = to_float(op(fixed_point_t{lhs}, fixed_point_t{rhs}));
         auto const expected = op(lhs, rhs);
-        return approx_equal(result, expected, tolerance);
+        return approx_equal(result, expected, tolerance<fixed_point_t>);
     };
 
     // operator+
@@ -194,57 +126,14 @@
     REQUIRE(binary_op(0.40F, 0.5F, std::multiplies()));
     REQUIRE(binary_op(0.45F, 0.5F, std::multiplies()));
     REQUIRE(binary_op(0.49F, 0.5F, std::multiplies()));
-
-    return true;
 }
 
-[[nodiscard]] static auto test_q15_t_binary_ops() -> bool
+TEMPLATE_TEST_CASE("neo/fft/fixed_point: comparison(fixed_point)", "", neo::fft::q7_t, neo::fft::q15_t)
 {
-    using fxp_t          = neo::fft::q15_t;
-    auto const tolerance = 0.0001F;
-
-    auto binary_op = [tolerance](auto lhs, auto rhs, auto op) {
-        auto const result   = to_float(op(fxp_t{lhs}, fxp_t{rhs}));
-        auto const expected = op(lhs, rhs);
-        return approx_equal(result, expected, tolerance);
-    };
-
-    // operator+
-    REQUIRE(binary_op(0.00F, 0.5F, std::plus()));
-    REQUIRE(binary_op(0.12F, 0.5F, std::plus()));
-    REQUIRE(binary_op(0.25F, 0.5F, std::plus()));
-    REQUIRE(binary_op(0.33F, 0.5F, std::plus()));
-    REQUIRE(binary_op(0.40F, 0.5F, std::plus()));
-    REQUIRE(binary_op(0.45F, 0.5F, std::plus()));
-    REQUIRE(binary_op(0.49F, 0.5F, std::plus()));
-
-    // operator-
-    REQUIRE(binary_op(0.00F, 0.5F, std::minus()));
-    REQUIRE(binary_op(0.12F, 0.5F, std::minus()));
-    REQUIRE(binary_op(0.25F, 0.5F, std::minus()));
-    REQUIRE(binary_op(0.33F, 0.5F, std::minus()));
-    REQUIRE(binary_op(0.40F, 0.5F, std::minus()));
-    REQUIRE(binary_op(0.45F, 0.5F, std::minus()));
-    REQUIRE(binary_op(0.49F, 0.5F, std::minus()));
-
-    // operator*
-    REQUIRE(binary_op(0.00F, 0.5F, std::multiplies()));
-    REQUIRE(binary_op(0.12F, 0.5F, std::multiplies()));
-    REQUIRE(binary_op(0.25F, 0.5F, std::multiplies()));
-    REQUIRE(binary_op(0.33F, 0.5F, std::multiplies()));
-    REQUIRE(binary_op(0.40F, 0.5F, std::multiplies()));
-    REQUIRE(binary_op(0.45F, 0.5F, std::multiplies()));
-    REQUIRE(binary_op(0.49F, 0.5F, std::multiplies()));
-
-    return true;
-}
-
-[[nodiscard]] static auto test_q7_t_comparison() -> bool
-{
-    using fxp_t = neo::fft::q7_t;
+    using fixed_point_t = TestType;
 
     auto compare_op = [](auto lhs, auto rhs, auto op) {
-        auto const result   = op(fxp_t{lhs}, fxp_t{rhs});
+        auto const result   = op(fixed_point_t{lhs}, fixed_point_t{rhs});
         auto const expected = op(lhs, rhs);
         return result == expected;
     };
@@ -284,63 +173,11 @@
     REQUIRE(compare_op(+0.50F, +0.00F, std::greater_equal()));
     REQUIRE(compare_op(+0.50F, -0.50F, std::greater_equal()));
     REQUIRE(compare_op(+0.50F, +0.50F, std::greater_equal()));
-
-    return true;
 }
 
-[[nodiscard]] static auto test_q15_t_comparison() -> bool
+TEMPLATE_TEST_CASE("neo/fft/fixed_point: add(fixed_point, fixed_point)", "", neo::fft::q7_t, neo::fft::q15_t)
 {
-    using fxp_t = neo::fft::q15_t;
-
-    auto compare_op = [](auto lhs, auto rhs, auto op) {
-        auto const result   = op(fxp_t{lhs}, fxp_t{rhs});
-        auto const expected = op(lhs, rhs);
-        return result == expected;
-    };
-
-    // operator==
-    REQUIRE(compare_op(+0.00F, +0.00F, std::equal_to()));
-    REQUIRE(compare_op(+0.50F, +0.00F, std::equal_to()));
-    REQUIRE(compare_op(+0.50F, -0.50F, std::equal_to()));
-    REQUIRE(compare_op(+0.50F, +0.50F, std::equal_to()));
-
-    // operator!=
-    REQUIRE(compare_op(+0.00F, +0.00F, std::not_equal_to()));
-    REQUIRE(compare_op(+0.50F, +0.00F, std::not_equal_to()));
-    REQUIRE(compare_op(+0.50F, -0.50F, std::not_equal_to()));
-    REQUIRE(compare_op(+0.50F, +0.50F, std::not_equal_to()));
-
-    // operator<
-    REQUIRE(compare_op(+0.00F, +0.00F, std::less()));
-    REQUIRE(compare_op(+0.50F, +0.00F, std::less()));
-    REQUIRE(compare_op(+0.50F, -0.50F, std::less()));
-    REQUIRE(compare_op(+0.50F, +0.50F, std::less()));
-
-    // operator<=
-    REQUIRE(compare_op(+0.00F, +0.00F, std::less_equal()));
-    REQUIRE(compare_op(+0.50F, +0.00F, std::less_equal()));
-    REQUIRE(compare_op(+0.50F, -0.50F, std::less_equal()));
-    REQUIRE(compare_op(+0.50F, +0.50F, std::less_equal()));
-
-    // operator>
-    REQUIRE(compare_op(+0.00F, +0.00F, std::greater()));
-    REQUIRE(compare_op(+0.50F, +0.00F, std::greater()));
-    REQUIRE(compare_op(+0.50F, -0.50F, std::greater()));
-    REQUIRE(compare_op(+0.50F, +0.50F, std::greater()));
-
-    // operator>=
-    REQUIRE(compare_op(+0.00F, +0.00F, std::greater_equal()));
-    REQUIRE(compare_op(+0.50F, +0.00F, std::greater_equal()));
-    REQUIRE(compare_op(+0.50F, -0.50F, std::greater_equal()));
-    REQUIRE(compare_op(+0.50F, +0.50F, std::greater_equal()));
-
-    return true;
-}
-
-[[nodiscard]] static auto test_q7_t_add() -> bool
-{
-    using fxp_t          = neo::fft::q7_t;
-    auto const tolerance = 0.01F;
+    using fxp_t = TestType;
 
     {
         // empty
@@ -357,7 +194,7 @@
         auto out = std::vector<fxp_t>(lhs.size(), fxp_t{});
         neo::fft::add(std::span{std::as_const(lhs)}, std::span{std::as_const(rhs)}, std::span{out});
 
-        auto eq = [=](auto fxp) { return approx_equal(to_float(fxp), 0.5F + 0.25F, tolerance); };
+        auto eq = [=](auto fxp) { return approx_equal(to_float(fxp), 0.5F + 0.25F, tolerance<TestType>); };
         REQUIRE(std::all_of(out.begin(), out.end(), eq));
     }
 
@@ -368,55 +205,14 @@
         auto out = std::vector<fxp_t>(lhs.size(), fxp_t{});
         neo::fft::add(std::span{std::as_const(lhs)}, std::span{std::as_const(rhs)}, std::span{out});
 
-        auto eq = [=](auto fxp) { return approx_equal(to_float(fxp), 0.125F + 0.25F, tolerance); };
+        auto eq = [=](auto fxp) { return approx_equal(to_float(fxp), 0.125F + 0.25F, tolerance<TestType>); };
         REQUIRE(std::all_of(out.begin(), out.end(), eq));
     }
-
-    return true;
 }
 
-[[nodiscard]] static auto test_q15_t_add() -> bool
+TEMPLATE_TEST_CASE("neo/fft/fixed_point: subtract(fixed_point, fixed_point)", "", neo::fft::q7_t, neo::fft::q15_t)
 {
-    using fxp_t          = neo::fft::q15_t;
-    auto const tolerance = 0.0001F;
-
-    {
-        // empty
-        auto lhs = std::vector<fxp_t>();
-        auto rhs = std::vector<fxp_t>();
-        auto out = std::vector<fxp_t>();
-        neo::fft::add(std::span{std::as_const(lhs)}, std::span{std::as_const(rhs)}, std::span{out});
-    }
-
-    {
-        // scalar
-        auto lhs = std::vector<fxp_t>{fxp_t{0.5}, fxp_t{0.5}, fxp_t{0.5}, fxp_t{0.5}};
-        auto rhs = std::vector<fxp_t>{fxp_t{0.25}, fxp_t{0.25}, fxp_t{0.25}, fxp_t{0.25}};
-        auto out = std::vector<fxp_t>(lhs.size(), fxp_t{});
-        neo::fft::add(std::span{std::as_const(lhs)}, std::span{std::as_const(rhs)}, std::span{out});
-
-        auto eq = [=](auto fxp) { return approx_equal(to_float(fxp), 0.5F + 0.25F, tolerance); };
-        REQUIRE(std::all_of(out.begin(), out.end(), eq));
-    }
-
-    {
-        // vectorized
-        auto lhs = std::vector<fxp_t>(1029, fxp_t{0.125F});
-        auto rhs = std::vector<fxp_t>(1029, fxp_t{0.25F});
-        auto out = std::vector<fxp_t>(lhs.size(), fxp_t{});
-        neo::fft::add(std::span{std::as_const(lhs)}, std::span{std::as_const(rhs)}, std::span{out});
-
-        auto eq = [=](auto fxp) { return approx_equal(to_float(fxp), 0.125F + 0.25F, tolerance); };
-        REQUIRE(std::all_of(out.begin(), out.end(), eq));
-    }
-
-    return true;
-}
-
-[[nodiscard]] static auto test_q7_t_subtract() -> bool
-{
-    using fxp_t          = neo::fft::q7_t;
-    auto const tolerance = 0.01F;
+    using fxp_t = TestType;
 
     {
         // empty
@@ -433,7 +229,7 @@
         auto out = std::vector<fxp_t>(lhs.size(), fxp_t{});
         neo::fft::subtract(std::span{std::as_const(lhs)}, std::span{std::as_const(rhs)}, std::span{out});
 
-        auto eq = [=](auto fxp) { return approx_equal(to_float(fxp), 0.5F - 0.25F, tolerance); };
+        auto eq = [=](auto fxp) { return approx_equal(to_float(fxp), 0.5F - 0.25F, tolerance<TestType>); };
         REQUIRE(std::all_of(out.begin(), out.end(), eq));
     }
 
@@ -444,55 +240,14 @@
         auto out = std::vector<fxp_t>(lhs.size(), fxp_t{});
         neo::fft::subtract(std::span{std::as_const(lhs)}, std::span{std::as_const(rhs)}, std::span{out});
 
-        auto eq = [=](auto fxp) { return approx_equal(to_float(fxp), 0.125F - 0.25F, tolerance); };
+        auto eq = [=](auto fxp) { return approx_equal(to_float(fxp), 0.125F - 0.25F, tolerance<TestType>); };
         REQUIRE(std::all_of(out.begin(), out.end(), eq));
     }
-
-    return true;
 }
 
-[[nodiscard]] static auto test_q15_t_subtract() -> bool
+TEMPLATE_TEST_CASE("neo/fft/fixed_point: multiply(fixed_point, fixed_point)", "", neo::fft::q7_t, neo::fft::q15_t)
 {
-    using fxp_t          = neo::fft::q15_t;
-    auto const tolerance = 0.0001F;
-
-    {
-        // empty
-        auto lhs = std::vector<fxp_t>();
-        auto rhs = std::vector<fxp_t>();
-        auto out = std::vector<fxp_t>();
-        neo::fft::subtract(std::span{std::as_const(lhs)}, std::span{std::as_const(rhs)}, std::span{out});
-    }
-
-    {
-        // scalar
-        auto lhs = std::vector<fxp_t>{fxp_t{0.5}, fxp_t{0.5}, fxp_t{0.5}, fxp_t{0.5}};
-        auto rhs = std::vector<fxp_t>{fxp_t{0.25}, fxp_t{0.25}, fxp_t{0.25}, fxp_t{0.25}};
-        auto out = std::vector<fxp_t>(lhs.size(), fxp_t{});
-        neo::fft::subtract(std::span{std::as_const(lhs)}, std::span{std::as_const(rhs)}, std::span{out});
-
-        auto eq = [=](auto fxp) { return approx_equal(to_float(fxp), 0.5F - 0.25F, tolerance); };
-        REQUIRE(std::all_of(out.begin(), out.end(), eq));
-    }
-
-    {
-        // vectorized
-        auto lhs = std::vector<fxp_t>(1029, fxp_t{0.125F});
-        auto rhs = std::vector<fxp_t>(1029, fxp_t{0.25F});
-        auto out = std::vector<fxp_t>(lhs.size(), fxp_t{});
-        neo::fft::subtract(std::span{std::as_const(lhs)}, std::span{std::as_const(rhs)}, std::span{out});
-
-        auto eq = [=](auto fxp) { return approx_equal(to_float(fxp), 0.125F - 0.25F, tolerance); };
-        REQUIRE(std::all_of(out.begin(), out.end(), eq));
-    }
-
-    return true;
-}
-
-[[nodiscard]] static auto test_q7_t_multiply() -> bool
-{
-    using fxp_t          = neo::fft::q7_t;
-    auto const tolerance = 0.01F;
+    using fxp_t = TestType;
 
     {
         // empty
@@ -509,7 +264,7 @@
         auto out = std::vector<fxp_t>(lhs.size(), fxp_t{});
         neo::fft::multiply(std::span{std::as_const(lhs)}, std::span{std::as_const(rhs)}, std::span{out});
 
-        auto eq = [=](auto fxp) { return approx_equal(to_float(fxp), 0.5F * 0.25F, tolerance); };
+        auto eq = [=](auto fxp) { return approx_equal(to_float(fxp), 0.5F * 0.25F, tolerance<TestType>); };
         REQUIRE(std::all_of(out.begin(), out.end(), eq));
     }
 
@@ -520,66 +275,7 @@
         auto out = std::vector<fxp_t>(lhs.size(), fxp_t{});
         neo::fft::multiply(std::span{std::as_const(lhs)}, std::span{std::as_const(rhs)}, std::span{out});
 
-        auto eq = [=](auto fxp) { return approx_equal(to_float(fxp), 0.125F * 0.25F, tolerance); };
+        auto eq = [=](auto fxp) { return approx_equal(to_float(fxp), 0.125F * 0.25F, tolerance<TestType>); };
         REQUIRE(std::all_of(out.begin(), out.end(), eq));
     }
-
-    return true;
-}
-
-[[nodiscard]] static auto test_q15_t_multiply() -> bool
-{
-    using fxp_t          = neo::fft::q15_t;
-    auto const tolerance = 0.0001F;
-
-    {
-        // empty
-        auto lhs = std::vector<fxp_t>();
-        auto rhs = std::vector<fxp_t>();
-        auto out = std::vector<fxp_t>();
-        neo::fft::multiply(std::span{std::as_const(lhs)}, std::span{std::as_const(rhs)}, std::span{out});
-    }
-
-    {
-        // scalar
-        auto lhs = std::vector<fxp_t>{fxp_t{0.5}, fxp_t{0.5}, fxp_t{0.5}, fxp_t{0.5}};
-        auto rhs = std::vector<fxp_t>{fxp_t{0.25}, fxp_t{0.25}, fxp_t{0.25}, fxp_t{0.25}};
-        auto out = std::vector<fxp_t>(lhs.size(), fxp_t{});
-        neo::fft::multiply(std::span{std::as_const(lhs)}, std::span{std::as_const(rhs)}, std::span{out});
-
-        auto eq = [=](auto fxp) { return approx_equal(to_float(fxp), 0.5F * 0.25F, tolerance); };
-        REQUIRE(std::all_of(out.begin(), out.end(), eq));
-    }
-
-    {
-        // vectorized
-        auto lhs = std::vector<fxp_t>(1029, fxp_t{0.125F});
-        auto rhs = std::vector<fxp_t>(1029, fxp_t{0.25F});
-        auto out = std::vector<fxp_t>(lhs.size(), fxp_t{});
-        neo::fft::multiply(std::span{std::as_const(lhs)}, std::span{std::as_const(rhs)}, std::span{out});
-
-        auto eq = [=](auto fxp) { return approx_equal(to_float(fxp), 0.125F * 0.25F, tolerance); };
-        REQUIRE(std::all_of(out.begin(), out.end(), eq));
-    }
-
-    return true;
-}
-
-TEST_CASE("neo/fft/fixed_point: fixed_point")
-{
-    REQUIRE(test_q7_t_conversion());
-    REQUIRE(test_q7_t_unary_ops());
-    REQUIRE(test_q7_t_binary_ops());
-    REQUIRE(test_q7_t_comparison());
-    REQUIRE(test_q7_t_add());
-    REQUIRE(test_q7_t_subtract());
-    REQUIRE(test_q7_t_multiply());
-
-    REQUIRE(test_q15_t_conversion());
-    REQUIRE(test_q15_t_unary_ops());
-    REQUIRE(test_q15_t_binary_ops());
-    REQUIRE(test_q15_t_comparison());
-    REQUIRE(test_q15_t_add());
-    REQUIRE(test_q15_t_subtract());
-    REQUIRE(test_q15_t_multiply());
 }
