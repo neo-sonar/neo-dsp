7540cb1d8e04a8b59d4ecd6039138f660dbdfddb
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-08-12 05:27:00 +0200

    Improve radix2 tests

--- src/lib/neo/fft/transform/radix2_test.cpp
+++ src/lib/neo/fft/transform/radix2_test.cpp
@@ -31,9 +31,10 @@ TEMPLATE_TEST_CASE("neo/fft/transform/radix2: make_radix2_twiddles", "", float, 
 
 TEMPLATE_TEST_CASE("neo/fft/transform/radix2: test_path(c2c)", "", double)
 {
-    using Float = TestType;
+    using Float   = TestType;
+    using Complex = std::complex<Float>;
 
-    auto paths = GENERATE(
+    auto const paths = GENERATE(
         neo::fft::test_path{"./test_data/c2c_8_input.csv", "./test_data/c2c_8_output.csv"},
         neo::fft::test_path{"./test_data/c2c_16_input.csv", "./test_data/c2c_16_output.csv"},
         neo::fft::test_path{"./test_data/c2c_32_input.csv", "./test_data/c2c_32_output.csv"},
@@ -47,6 +48,7 @@ TEMPLATE_TEST_CASE("neo/fft/transform/radix2: test_path(c2c)", "", double)
     auto const testCase = neo::fft::load_test_data<Float>(paths);
     auto const expected = Kokkos::mdspan{testCase.expected.data(), Kokkos::extents{testCase.expected.size()}};
 
+    SECTION("dft")
     {
         auto in  = testCase.input;
         auto out = std::vector<std::complex<Float>>(in.size());
@@ -58,28 +60,39 @@ TEMPLATE_TEST_CASE("neo/fft/transform/radix2: test_path(c2c)", "", double)
         REQUIRE(neo::fft::allclose(expected, outVec));
     }
 
+    SECTION("kernel")
     {
-        auto inout = testCase.input;
-        auto io    = Kokkos::mdspan{inout.data(), Kokkos::extents{inout.size()}};
-        auto tw    = neo::fft::make_radix2_twiddles<std::complex<Float>>(inout.size());
-        neo::fft::fft_radix2(neo::fft::fft_radix2_kernel_v1, io, tw);
-        REQUIRE(neo::fft::allclose(expected, io));
+        auto test = [=](auto kernel) {
+            auto inout = testCase.input;
+            auto io    = Kokkos::mdspan{inout.data(), Kokkos::extents{inout.size()}};
+
+            auto tw = neo::fft::make_radix2_twiddles<Complex>(inout.size());
+            neo::fft::fft_radix2(neo::fft::fft_radix2_kernel_v1, io, tw);
+
+            return neo::fft::allclose(expected, io);
+        };
+
+        REQUIRE(test(neo::fft::fft_radix2_kernel_v1));
+        REQUIRE(test(neo::fft::fft_radix2_kernel_v2));
+        REQUIRE(test(neo::fft::fft_radix2_kernel_v3));
     }
 
+    SECTION("plan")
     {
-        auto inout = testCase.input;
-        auto io    = Kokkos::mdspan{inout.data(), Kokkos::extents{inout.size()}};
-        auto tw    = neo::fft::make_radix2_twiddles<std::complex<Float>>(inout.size());
-        neo::fft::fft_radix2(neo::fft::fft_radix2_kernel_v2, io, tw);
-        REQUIRE(neo::fft::allclose(expected, io));
-    }
+        auto test = [=](auto kernel) {
+            auto inout = testCase.input;
+            auto io    = Kokkos::mdspan{inout.data(), Kokkos::extents{inout.size()}};
 
-    {
-        auto inout = testCase.input;
-        auto io    = Kokkos::mdspan{inout.data(), Kokkos::extents{inout.size()}};
-        auto plan  = neo::fft::fft_radix2_plan<std::complex<Float>>{neo::fft::ilog2(inout.size())};
-        plan(io, neo::fft::direction::forward);
-        REQUIRE(neo::fft::allclose(expected, io));
+            auto order = neo::fft::ilog2(inout.size());
+            auto plan  = neo::fft::fft_radix2_plan<Complex, decltype(kernel)>{order};
+
+            plan(io, neo::fft::direction::forward);
+            return neo::fft::allclose(expected, io);
+        };
+
+        REQUIRE(test(neo::fft::fft_radix2_kernel_v1));
+        REQUIRE(test(neo::fft::fft_radix2_kernel_v2));
+        REQUIRE(test(neo::fft::fft_radix2_kernel_v3));
     }
 }
 
@@ -87,21 +100,30 @@ TEMPLATE_TEST_CASE("neo/fft/transform/radix2: roundtrip(fft_radix2_plan)", "", f
 {
     using Float = TestType;
 
-    auto const size = GENERATE(as<size_t>{}, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384);
+    auto const size  = GENERATE(as<size_t>{}, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384);
+    auto const order = neo::fft::ilog2(size);
 
-    auto const original = neo::fft::generate_noise_signal<std::complex<Float>>(size, Catch::getSeed());
-    auto const noise    = Kokkos::mdspan{original.data(), Kokkos::extents{original.size()}};
+    auto test = [=](auto kernel) {
+        auto const original = neo::fft::generate_noise_signal<std::complex<Float>>(size, Catch::getSeed());
+        auto const noise    = Kokkos::mdspan{original.data(), Kokkos::extents{original.size()}};
 
-    auto copy = original;
-    auto io   = Kokkos::mdspan{copy.data(), Kokkos::extents{copy.size()}};
+        auto copy = original;
+        auto io   = Kokkos::mdspan{copy.data(), Kokkos::extents{copy.size()}};
 
-    auto plan = neo::fft::fft_radix2_plan<std::complex<Float>>{neo::fft::ilog2(size)};
-    REQUIRE(plan.size() == size);
-    REQUIRE(plan.order() == neo::fft::ilog2(size));
+        auto plan = neo::fft::fft_radix2_plan<std::complex<Float>, decltype(kernel)>{order};
+        REQUIRE(plan.size() == size);
+        REQUIRE(plan.order() == order);
 
-    plan(io, neo::fft::direction::forward);
-    plan(io, neo::fft::direction::backward);
-    neo::fft::scale(Float(1) / static_cast<Float>(plan.size()), io);
+        plan(io, neo::fft::direction::forward);
+        plan(io, neo::fft::direction::backward);
 
-    REQUIRE(neo::fft::allclose(noise, io));
+        neo::fft::scale(Float(1) / static_cast<Float>(plan.size()), io);
+        REQUIRE(neo::fft::allclose(noise, io));
+
+        return true;
+    };
+
+    REQUIRE(test(neo::fft::fft_radix2_kernel_v1));
+    REQUIRE(test(neo::fft::fft_radix2_kernel_v2));
+    REQUIRE(test(neo::fft::fft_radix2_kernel_v3));
 }
