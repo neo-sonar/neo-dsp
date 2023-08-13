01cbc1b6b88ffd2475dcc2e1c7a20707101ede62
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-08-13 21:55:04 +0200

    Improve simd test names

--- src/lib/neo/math/simd_test.cpp
+++ src/lib/neo/math/simd_test.cpp
@@ -6,48 +6,28 @@
 
 #include <array>
 
-#if defined(NEO_HAS_SIMD_SSE2)
-
-TEST_CASE("neo/math: simd::float32x4")
+template<typename Batch>
+static auto test_float_batch()
 {
-    auto output = std::array<float, 4>{};
-    auto reg    = neo::simd::float32x4::broadcast(1.0F);
+    using Float = typename Batch::value_type;
+
+    auto const reg = Batch::broadcast(Float(42));
+    auto output    = std::array<Float, Batch::size>{};
     reg.store_unaligned(output.data());
+
     for (auto val : output) {
-        REQUIRE(val == Catch::Approx(1.0));
+        REQUIRE(val == Catch::Approx(42.0));
     }
 }
 
-TEST_CASE("neo/math: simd::float64x2")
+template<typename ComplexBatch>
+static auto test_complex_batch()
 {
-    auto output = std::array<double, 2>{};
-    auto reg    = neo::simd::float64x2::broadcast(1.0);
-    reg.store_unaligned(output.data());
-    for (auto val : output) {
-        REQUIRE(val == Catch::Approx(1.0));
-    }
-}
-
-using complex_types = std::tuple<
-    #if defined(NEO_HAS_SIMD_AVX512F)
-    neo::simd::complex32x8,
-    neo::simd::complex64x4,
-    #endif
-    #if defined(NEO_HAS_SIMD_AVX)
-    neo::simd::complex32x4,
-    neo::simd::complex64x2,
-    #endif
-    neo::simd::complex32x2,
-    neo::simd::complex64x1>;
-
-TEMPLATE_LIST_TEST_CASE("neo/math: arithmetic", "", complex_types)
-{
-    using Complex    = TestType;
-    using FloatBatch = typename Complex::batch_type;
+    using FloatBatch = typename ComplexBatch::batch_type;
     using Float      = typename FloatBatch::value_type;
 
     auto test = [](auto op, auto left_val, auto right_val, auto expected) -> void {
-        static constexpr auto const size = Complex::size;
+        static constexpr auto const size = ComplexBatch::size;
 
         auto left = std::array<std::complex<Float>, size>{};
         std::fill(left.begin(), left.end(), left_val);
@@ -55,8 +35,8 @@ TEMPLATE_LIST_TEST_CASE("neo/math: arithmetic", "", complex_types)
         auto right = std::array<std::complex<Float>, size>{};
         std::fill(right.begin(), right.end(), right_val);
 
-        auto lhs    = Complex::load_unaligned(left.data());
-        auto rhs    = Complex::load_unaligned(right.data());
+        auto lhs    = ComplexBatch::load_unaligned(left.data());
+        auto rhs    = ComplexBatch::load_unaligned(right.data());
         auto result = op(lhs, rhs);
 
         auto output = std::array<std::complex<Float>, size>{};
@@ -76,6 +56,46 @@ TEMPLATE_LIST_TEST_CASE("neo/math: arithmetic", "", complex_types)
 
     // auto const identity = std::complex{Float(1), Float(0)};
     // test(std::multiplies{}, threes, identity, threes);
+}
+
+#if defined(NEO_HAS_SIMD_SSE2)
+
+TEMPLATE_TEST_CASE("neo/math: float_batch", "", neo::simd::float32x4, neo::simd::float64x2)
+{
+    test_float_batch<TestType>();
+}
+
+TEMPLATE_TEST_CASE("neo/math: complex_batch", "", neo::simd::complex32x2, neo::simd::complex64x1)
+{
+    test_complex_batch<TestType>();
+}
+
+#endif
+
+#if defined(NEO_HAS_SIMD_AVX)
+
+TEMPLATE_TEST_CASE("neo/math: float_batch", "", neo::simd::float32x8, neo::simd::float64x4)
+{
+    test_float_batch<TestType>();
+}
+
+TEMPLATE_TEST_CASE("neo/math: complex_batch", "", neo::simd::complex32x4, neo::simd::complex64x2)
+{
+    test_complex_batch<TestType>();
+}
+
+#endif
+
+#if defined(NEO_HAS_SIMD_AVX512F)
+
+TEMPLATE_TEST_CASE("neo/math: float_batch", "", neo::simd::float32x16, neo::simd::float64x8)
+{
+    test_float_batch<TestType>();
+}
+
+TEMPLATE_TEST_CASE("neo/math: complex_batch", "", neo::simd::complex32x8, neo::simd::complex64x4)
+{
+    test_complex_batch<TestType>();
 }
 
 #endif
