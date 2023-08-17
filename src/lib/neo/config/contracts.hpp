407a5689db6f473e7254c141db6d3b9efc7bbebe
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-08-17 17:05:12 +0200

    Turn on exceptions for contract violations

--- src/lib/neo/config/contracts.hpp
+++ src/lib/neo/config/contracts.hpp
@@ -16,7 +16,7 @@ enum struct contracts_check_mode
 };
 
 #if defined(_DEBUG) || !defined(NDEBUG)
-inline constexpr auto current_contracts_check_mode = contracts_check_mode::assertion;
+inline constexpr auto current_contracts_check_mode = contracts_check_mode::exception;
 #else
 inline constexpr auto current_contracts_check_mode = contracts_check_mode::exception;
 #endif
