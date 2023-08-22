fb87c35b11f4e099326698e31552a6a7b94bb583
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-08-22 07:10:57 +0200

    Add uniform_partitioner

--- src/lib/neo/fft/convolution/uniform_partition.hpp
+++ src/lib/neo/fft/convolution/uniform_partition.hpp
@@ -12,47 +12,66 @@
 
 namespace neo::fft {
 
-template<in_matrix InMat>
-[[nodiscard]] auto uniform_partition(InMat buffer, std::size_t block_size)
+template<complex Complex>
+struct uniform_partitioner
 {
-    using Float = typename InMat::value_type;
+    using value_type = Complex;
+    using real_type  = typename Complex::value_type;
+    using size_type  = std::size_t;
 
-    auto const window_size    = block_size * 2;
-    auto const num_bins       = block_size + 1;
-    auto const num_channels   = buffer.extent(0);
-    auto const num_partitions = divide_round_up(buffer.extent(1), block_size);
+    explicit uniform_partitioner(size_type block_size) : _block_size{block_size} {}
 
-    auto partitions = stdex::mdarray<std::complex<Float>, stdex::dextents<std::size_t, 3>>{
-        num_channels,
-        num_partitions,
-        num_bins,
-    };
+    template<in_matrix InMat>
+        requires std::convertible_to<typename InMat::value_type, real_type>
+    auto operator()(InMat impulse_response)
+    {
+        auto const num_channels   = impulse_response.extent(0);
+        auto const num_partitions = divide_round_up(impulse_response.extent(1), _block_size);
 
-    auto rfft = rfft_radix2_plan<Float>{ilog2(window_size)};
-    auto in   = stdex::mdarray<Float, stdex::dextents<std::size_t, 1>>{rfft.size()};
-    auto out  = stdex::mdarray<std::complex<Float>, stdex::dextents<std::size_t, 1>>{rfft.size()};
+        auto partitions = stdex::mdarray<Complex, stdex::dextents<size_type, 3>>{
+            num_channels,
+            num_partitions,
+            _num_bins,
+        };
 
-    auto const input  = in.to_mdspan();
-    auto const output = out.to_mdspan();
+        auto const input  = _in.to_mdspan();
+        auto const output = _out.to_mdspan();
 
-    for (auto channel{0UL}; channel < num_channels; ++channel) {
-        for (auto partition_idx{0UL}; partition_idx < num_partitions; ++partition_idx) {
-            auto const idx         = partition_idx * block_size;
-            auto const num_samples = std::min(buffer.extent(1) - idx, block_size);
-            auto const block       = stdex::submdspan(buffer, channel, std::tuple{idx, idx + num_samples});
+        for (auto ch{0UL}; ch < num_channels; ++ch) {
+            for (auto partition_idx{0UL}; partition_idx < num_partitions; ++partition_idx) {
+                auto const idx         = partition_idx * _block_size;
+                auto const num_samples = std::min(impulse_response.extent(1) - idx, _block_size);
+                auto const block       = stdex::submdspan(impulse_response, ch, std::tuple{idx, idx + num_samples});
 
-            fill(input, Float(0));
-            fill(output, std::complex<Float>(0));
-            copy(block, stdex::submdspan(input, std::tuple{0, num_samples}));
-            rfft(input, output);
+                fill(input, real_type(0));
+                fill(output, Complex(0));
+                copy(block, stdex::submdspan(input, std::tuple{0, num_samples}));
+                _rfft(input, output);
 
-            auto const coeffs    = stdex::submdspan(output, std::tuple{0, num_bins});
-            auto const partition = stdex::submdspan(partitions.to_mdspan(), channel, partition_idx, stdex::full_extent);
-            copy(coeffs, partition);
+                auto const coeffs    = stdex::submdspan(output, std::tuple{0, _num_bins});
+                auto const partition = stdex::submdspan(partitions.to_mdspan(), ch, partition_idx, stdex::full_extent);
+                copy(coeffs, partition);
+            }
         }
+
+        return partitions;
     }
 
-    return partitions;
+private:
+    size_type _block_size;
+    size_type _window_size{_block_size * 2};
+    size_type _num_bins{_block_size + 1};
+
+    rfft_radix2_plan<real_type> _rfft{ilog2(_window_size)};
+    stdex::mdarray<real_type, stdex::dextents<size_type, 1>> _in{_rfft.size()};
+    stdex::mdarray<Complex, stdex::dextents<size_type, 1>> _out{_rfft.size()};
+};
+
+template<in_matrix InMat>
+[[nodiscard]] auto uniform_partition(InMat impulse_response, std::size_t block_size)
+{
+    using Complex = std::complex<typename InMat::value_type>;
+    return uniform_partitioner<Complex>{block_size}(impulse_response);
 }
 
 }  // namespace neo::fft
