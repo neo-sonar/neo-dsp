8b85f73feed2e12683f33fff324af11de669cc2e
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-08-23 22:13:02 +0200

    Add stft_options

--- src/lib/neo/fft/transform/stft.hpp
+++ src/lib/neo/fft/transform/stft.hpp
@@ -11,44 +11,84 @@
 
 namespace neo::fft {
 
-template<in_matrix InMat>
-[[nodiscard]] auto stft(InMat buffer, int windowSize)
-    -> stdex::mdarray<std::complex<typename InMat::value_type>, stdex::dextents<size_t, 2>>
+namespace detail {
+
+[[nodiscard]] constexpr auto num_sftf_segments(int signal_len, int segment_len, int overlap_len) noexcept
 {
+    return 1 + idiv((signal_len - segment_len + overlap_len), (segment_len - overlap_len));
+}
+
+static_assert(num_sftf_segments(1024, 128, 0) == 8);
+static_assert(num_sftf_segments(1024, 256, 0) == 4);
+static_assert(num_sftf_segments(1024, 256, 128) == 8);
+
+}  // namespace detail
+
+struct stft_options
+{
+    int segment_length;
+    int overlap_length;
+    int transform_size;
+};
+
+template<in_matrix InMat>
+[[nodiscard]] auto stft(InMat x, stft_options options)
+{
+    assert(options.segment_length == options.transform_size);
+
     using Float = typename InMat::value_type;
 
-    auto fft       = rfft_radix2_plan<Float>{ilog2(static_cast<size_t>(windowSize))};
-    auto fftInput  = stdex::mdarray<Float, stdex::dextents<std::size_t, 1>>{fft.size()};
-    auto fftOutput = stdex::mdarray<std::complex<Float>, stdex::dextents<std::size_t, 1>>{fft.size()};
-    auto hann      = generate_window<Float>(static_cast<size_t>(windowSize));
+    auto rfft       = rfft_radix2_plan<Float>{static_cast<size_t>(ilog2(options.transform_size))};
+    auto fft_input  = stdex::mdarray<Float, stdex::dextents<std::size_t, 1>>{rfft.size()};
+    auto fft_output = stdex::mdarray<std::complex<Float>, stdex::dextents<std::size_t, 1>>{rfft.size()};
+    auto hann       = generate_window<Float>(rfft.size());
+    auto scalar     = Float(1) / static_cast<Float>(rfft.size());
 
-    auto const totalNumSamples = static_cast<int>(buffer.extent(1));
-    auto const numBins         = static_cast<std::size_t>(windowSize / 2 + 1);
-    auto const numFrames       = static_cast<std::size_t>(idiv(totalNumSamples, windowSize));
+    auto const total_num_samples = static_cast<int>(x.extent(1));
+    auto const num_bins          = rfft.size() / 2UL + 1UL;
+    auto const num_segments      = static_cast<std::size_t>(
+        detail::num_sftf_segments(total_num_samples, options.segment_length, options.overlap_length)
+    );
 
-    auto result = stdex::mdarray<std::complex<Float>, stdex::dextents<size_t, 2>>{numFrames, numBins};
+    auto result = stdex::mdarray<std::complex<Float>, stdex::dextents<size_t, 2>>{num_segments, num_bins};
 
-    for (auto frameIdx{0UL}; frameIdx < result.extent(0); ++frameIdx) {
-        auto const idx        = static_cast<int>(frameIdx * result.extent(1));
-        auto const numSamples = std::min(totalNumSamples - idx, windowSize);
-        auto const channel    = 0;
+    for (auto segment_idx{0UL}; segment_idx < result.extent(0); ++segment_idx) {
+        fill(fft_input.to_mdspan(), Float(0));
+        fill(fft_output.to_mdspan(), Float(0));
 
-        auto block  = stdex::submdspan(buffer, channel, std::tuple{idx, idx + numSamples});
-        auto window = stdex::submdspan(fftInput.to_mdspan(), std::tuple{0, numSamples});
-        auto coeffs = stdex::submdspan(fftOutput.to_mdspan(), std::tuple{0, result.extent(1)});
-        auto frame  = stdex::submdspan(result.to_mdspan(), frameIdx, std::tuple{0, result.extent(1)});
+        auto const idx         = static_cast<int>(segment_idx) * options.overlap_length;
+        auto const num_samples = std::min(total_num_samples - idx, options.segment_length);
+        auto const channel     = 0;
 
-        fill(fftInput.to_mdspan(), Float(0));
-        fill(fftOutput.to_mdspan(), Float(0));
+        auto block  = stdex::submdspan(x, channel, std::tuple{idx, idx + num_samples});
+        auto window = stdex::submdspan(fft_input.to_mdspan(), std::tuple{0, num_samples});
 
-        multiply(block, hann.to_mdspan(), window);
-        fft(window, fftOutput.to_mdspan());
+        copy(block, window);
+        multiply(fft_input.to_mdspan(), hann.to_mdspan(), fft_input.to_mdspan());
+        rfft(fft_input.to_mdspan(), fft_output.to_mdspan());
 
-        scale(Float(1) / static_cast<Float>(windowSize), coeffs);
-        copy(coeffs, frame);
+        auto coeffs  = stdex::submdspan(fft_output.to_mdspan(), std::tuple{0, result.extent(1)});
+        auto segment = stdex::submdspan(result.to_mdspan(), segment_idx, std::tuple{0, result.extent(1)});
+
+        scale(scalar, coeffs);
+        copy(coeffs, segment);
     }
 
     return result;
+}
+
+template<in_matrix InMat>
+[[nodiscard]] auto stft(InMat x, int window_size)
+    -> stdex::mdarray<std::complex<typename InMat::value_type>, stdex::dextents<size_t, 2>>
+{
+    return stft(
+        x,
+        stft_options{
+            .segment_length = window_size,
+            .overlap_length = window_size / 2,
+            .transform_size = window_size,
+        }
+    );
 }
 
 }  // namespace neo::fft
