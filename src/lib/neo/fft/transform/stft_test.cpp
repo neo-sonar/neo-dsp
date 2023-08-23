c8eb83559446163782637b11bd9c3fd5e06bedd2
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-08-23 22:21:23 +0200

    Fix stft tests

--- src/lib/neo/fft/transform/stft_test.cpp
+++ src/lib/neo/fft/transform/stft_test.cpp
@@ -9,17 +9,31 @@ TEMPLATE_TEST_CASE("neo/fft/transform: stft", "", float, double, long double)
 
     SECTION("no remainder")
     {
-        auto signal = stdex::mdarray<Float, stdex::dextents<std::size_t, 2>>{1, 2048};
-        auto frames = neo::fft::stft(signal.to_mdspan(), 256);
-        REQUIRE(frames.extent(0) == 8);
-        REQUIRE(frames.extent(1) == 129);
+        auto signal   = stdex::mdarray<Float, stdex::dextents<std::size_t, 2>>{1, 2048};
+        auto segments = neo::fft::stft(
+            signal.to_mdspan(),
+            neo::fft::stft_options{
+                .segment_length = 256,
+                .overlap_length = 0,
+                .transform_size = 256,
+            }
+        );
+        REQUIRE(segments.extent(0) == 8);
+        REQUIRE(segments.extent(1) == 129);
     }
 
     SECTION("with remainder")
     {
-        auto signal = stdex::mdarray<Float, stdex::dextents<std::size_t, 2>>{1, 2040};
-        auto frames = neo::fft::stft(signal.to_mdspan(), 256);
-        REQUIRE(frames.extent(0) == 8);
-        REQUIRE(frames.extent(1) == 129);
+        auto signal   = stdex::mdarray<Float, stdex::dextents<std::size_t, 2>>{1, 2040};
+        auto segments = neo::fft::stft(
+            signal.to_mdspan(),
+            neo::fft::stft_options{
+                .segment_length = 256,
+                .overlap_length = 0,
+                .transform_size = 256,
+            }
+        );
+        REQUIRE(segments.extent(0) == 8);
+        REQUIRE(segments.extent(1) == 129);
     }
 }
