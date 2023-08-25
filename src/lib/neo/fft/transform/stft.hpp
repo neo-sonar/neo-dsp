e39f323d170c44d32599b8d7ebe5ea58854aa6ef
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-08-25 10:25:04 +0200

    Add stft_plan

--- src/lib/neo/fft/transform/stft.hpp
+++ src/lib/neo/fft/transform/stft.hpp
@@ -13,82 +13,94 @@ namespace neo::fft {
 
 namespace detail {
 
-[[nodiscard]] constexpr auto num_sftf_segments(int signal_len, int segment_len, int overlap_len) noexcept
+[[nodiscard]] constexpr auto num_sftf_frames(int signal_len, int frame_len, int overlap_len) noexcept
 {
-    return 1 + idiv((signal_len - segment_len + overlap_len), (segment_len - overlap_len));
+    return 1 + idiv((signal_len - frame_len + overlap_len), (frame_len - overlap_len));
 }
-
-static_assert(num_sftf_segments(1024, 128, 0) == 8);
-static_assert(num_sftf_segments(1024, 256, 0) == 4);
-static_assert(num_sftf_segments(1024, 256, 128) == 8);
 
 }  // namespace detail
 
 struct stft_options
 {
-    int segment_length;
+    int frame_length;
     int overlap_length;
     int transform_size;
+};
+
+template<std::floating_point Float>
+struct stft_plan
+{
+    explicit stft_plan(int transform_size)
+        : _options{
+            .frame_length   = transform_size,
+            .overlap_length = transform_size / 2,
+            .transform_size = transform_size,
+        }
+    {}
+
+    explicit stft_plan(stft_options options) : _options{options} {}
+
+    template<in_matrix InMat>
+    [[nodiscard]] auto operator()(InMat x)
+    {
+        auto const scalar            = Float(1) / static_cast<Float>(_rfft.size());
+        auto const total_num_samples = static_cast<int>(x.extent(1));
+        auto const num_bins          = _rfft.size() / 2UL + 1UL;
+        auto const num_frames        = static_cast<std::size_t>(
+            detail::num_sftf_frames(total_num_samples, _options.frame_length, _options.overlap_length)
+        );
+
+        auto result = stdex::mdarray<std::complex<Float>, stdex::dextents<size_t, 2>>{
+            num_frames,
+            num_bins,
+        };
+
+        for (auto frame_idx{0UL}; frame_idx < result.extent(0); ++frame_idx) {
+            fill(_fft_input.to_mdspan(), Float(0));
+            fill(_fft_output.to_mdspan(), Float(0));
+
+            auto const idx         = static_cast<int>(frame_idx) * _options.overlap_length;
+            auto const num_samples = std::min(total_num_samples - idx, _options.frame_length);
+            auto const channel     = 0;
+
+            auto block  = stdex::submdspan(x, channel, std::tuple{idx, idx + num_samples});
+            auto window = stdex::submdspan(_fft_input.to_mdspan(), std::tuple{0, num_samples});
+
+            copy(block, window);
+            multiply(_fft_input.to_mdspan(), _hann.to_mdspan(), _fft_input.to_mdspan());
+            _rfft(_fft_input.to_mdspan(), _fft_output.to_mdspan());
+
+            auto coeffs = stdex::submdspan(_fft_output.to_mdspan(), std::tuple{0, result.extent(1)});
+            auto frame  = stdex::submdspan(result.to_mdspan(), frame_idx, std::tuple{0, result.extent(1)});
+
+            scale(scalar, coeffs);
+            copy(coeffs, frame);
+        }
+
+        return result;
+    }
+
+private:
+    stft_options _options;
+    rfft_radix2_plan<Float> _rfft{static_cast<size_t>(ilog2(_options.transform_size))};
+    stdex::mdarray<Float, stdex::dextents<std::size_t, 1>> _fft_input{_rfft.size()};
+    stdex::mdarray<std::complex<Float>, stdex::dextents<std::size_t, 1>> _fft_output{_rfft.size()};
+    stdex::mdarray<Float, stdex::dextents<std::size_t, 1>> _hann{generate_window<Float>(_rfft.size())};
 };
 
 template<in_matrix InMat>
 [[nodiscard]] auto stft(InMat x, stft_options options)
 {
-    assert(options.segment_length == options.transform_size);
-
-    using Float = typename InMat::value_type;
-
-    auto rfft       = rfft_radix2_plan<Float>{static_cast<size_t>(ilog2(options.transform_size))};
-    auto fft_input  = stdex::mdarray<Float, stdex::dextents<std::size_t, 1>>{rfft.size()};
-    auto fft_output = stdex::mdarray<std::complex<Float>, stdex::dextents<std::size_t, 1>>{rfft.size()};
-    auto hann       = generate_window<Float>(rfft.size());
-    auto scalar     = Float(1) / static_cast<Float>(rfft.size());
-
-    auto const total_num_samples = static_cast<int>(x.extent(1));
-    auto const num_bins          = rfft.size() / 2UL + 1UL;
-    auto const num_segments      = static_cast<std::size_t>(
-        detail::num_sftf_segments(total_num_samples, options.segment_length, options.overlap_length)
-    );
-
-    auto result = stdex::mdarray<std::complex<Float>, stdex::dextents<size_t, 2>>{num_segments, num_bins};
-
-    for (auto segment_idx{0UL}; segment_idx < result.extent(0); ++segment_idx) {
-        fill(fft_input.to_mdspan(), Float(0));
-        fill(fft_output.to_mdspan(), Float(0));
-
-        auto const idx         = static_cast<int>(segment_idx) * options.overlap_length;
-        auto const num_samples = std::min(total_num_samples - idx, options.segment_length);
-        auto const channel     = 0;
-
-        auto block  = stdex::submdspan(x, channel, std::tuple{idx, idx + num_samples});
-        auto window = stdex::submdspan(fft_input.to_mdspan(), std::tuple{0, num_samples});
-
-        copy(block, window);
-        multiply(fft_input.to_mdspan(), hann.to_mdspan(), fft_input.to_mdspan());
-        rfft(fft_input.to_mdspan(), fft_output.to_mdspan());
-
-        auto coeffs  = stdex::submdspan(fft_output.to_mdspan(), std::tuple{0, result.extent(1)});
-        auto segment = stdex::submdspan(result.to_mdspan(), segment_idx, std::tuple{0, result.extent(1)});
-
-        scale(scalar, coeffs);
-        copy(coeffs, segment);
-    }
-
-    return result;
+    auto plan = stft_plan<typename InMat::value_type>{options};
+    return plan(x);
 }
 
 template<in_matrix InMat>
 [[nodiscard]] auto stft(InMat x, int window_size)
     -> stdex::mdarray<std::complex<typename InMat::value_type>, stdex::dextents<size_t, 2>>
 {
-    return stft(
-        x,
-        stft_options{
-            .segment_length = window_size,
-            .overlap_length = window_size / 2,
-            .transform_size = window_size,
-        }
-    );
+    auto plan = stft_plan<typename InMat::value_type>{window_size};
+    return plan(x);
 }
 
 }  // namespace neo::fft
--- src/lib/neo/fft/transform/stft_test.cpp
+++ src/lib/neo/fft/transform/stft_test.cpp
@@ -1,39 +1,33 @@
 #include "stft.hpp"
 
-#include <catch2/catch_approx.hpp>
 #include <catch2/catch_template_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+
+TEST_CASE("neo/fft/transform: detail::num_sftf_frames")
+{
+    REQUIRE(neo::fft::detail::num_sftf_frames(1024, 128, 0) == 8);
+    REQUIRE(neo::fft::detail::num_sftf_frames(1024, 256, 0) == 4);
+    REQUIRE(neo::fft::detail::num_sftf_frames(1024, 256, 128) == 8);
+}
 
 TEMPLATE_TEST_CASE("neo/fft/transform: stft", "", float, double)
 {
     using Float = TestType;
 
-    SECTION("no remainder")
-    {
-        auto signal   = stdex::mdarray<Float, stdex::dextents<std::size_t, 2>>{1, 2048};
-        auto segments = neo::fft::stft(
-            signal.to_mdspan(),
-            neo::fft::stft_options{
-                .segment_length = 256,
-                .overlap_length = 0,
-                .transform_size = 256,
-            }
-        );
-        REQUIRE(segments.extent(0) == 8);
-        REQUIRE(segments.extent(1) == 129);
-    }
+    auto const size   = GENERATE(as<std::size_t>{}, 2040, 2048);
+    auto const signal = stdex::mdarray<Float, stdex::dextents<std::size_t, 2>>{1, size};
 
-    SECTION("with remainder")
-    {
-        auto signal   = stdex::mdarray<Float, stdex::dextents<std::size_t, 2>>{1, 2040};
-        auto segments = neo::fft::stft(
-            signal.to_mdspan(),
-            neo::fft::stft_options{
-                .segment_length = 256,
-                .overlap_length = 0,
-                .transform_size = 256,
-            }
-        );
-        REQUIRE(segments.extent(0) == 8);
-        REQUIRE(segments.extent(1) == 129);
-    }
+    auto const no_overlap_options = neo::fft::stft_options{
+        .frame_length   = 256,
+        .overlap_length = 0,
+        .transform_size = 256,
+    };
+
+    auto no_overlap = neo::fft::stft(signal.to_mdspan(), no_overlap_options);
+    REQUIRE(no_overlap.extent(0) == 8);
+    REQUIRE(no_overlap.extent(1) == 129);
+
+    auto half_overlap = neo::fft::stft(signal.to_mdspan(), 256);
+    REQUIRE(half_overlap.extent(0) == 16);
+    REQUIRE(half_overlap.extent(1) == 129);
 }
