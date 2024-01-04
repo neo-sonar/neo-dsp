cd833420487cabe1744c8ba101c68d02973f5936
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-01-04 03:39:27 +0100

    Remove parrallel & interleave complex

--- src/neo/complex.hpp
+++ src/neo/complex.hpp
@@ -3,7 +3,5 @@
 #pragma once
 
 #include <neo/complex/complex.hpp>
-#include <neo/complex/interleave_complex.hpp>
-#include <neo/complex/parallel_complex.hpp>
 #include <neo/complex/scalar_complex.hpp>
 #include <neo/complex/split_complex.hpp>
